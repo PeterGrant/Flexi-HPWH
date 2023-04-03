c25a094343d16c7526741e871706b1bf30a9b37e
Peter <pgrant@lbl.gov>
2023-04-03 15:06:52 -0700

    Updated unit conversion code

---
+++ Utilities/Conversions.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  6 15:11:55 2021
+
+@author: Peter Grant
+"""
+
+seconds_in_minute = 60
+minutes_in_hour = 60
+hours_in_day = 24
+W_in_BtuPerHour = 0.293071
+kWh_in_Btu = 0.000293071
+therm_in_kWh = 0.0341296
+W_in_kW = 1000
+L_in_m3 = 1000
+J_in_kWh = 3600000
+gal_in_m3 = 264.172
+L_in_gal = 3.78541
